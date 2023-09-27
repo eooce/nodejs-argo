5ebd4355fac095a51d1581200e62602ec469bed9
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-27 17:31:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
+const { spawn } = require('child_process');
 const fs = require('fs');
-const http = require('http');
-const { exec } = require('child_process');
-const port = process.env.PORT || 3000;
-const filePath = './server'; 
-const newPermissions = 0o775; 
 
-fs.chmod(filePath, newPermissions, (err) => {
-  if (err) {
-    console.error(`更改权限时发生错误: ${err}`);
-  } else {
-    console.log(`成功更改文件权限为 ${newPermissions.toString(8)} (${newPermissions.toString(10)})`);
-  }
+const startScriptPath = './start.sh';
+const interpreterPath = '/usr/bin/env';
+const interpreterArgs = ['bash', startScriptPath];
+
+try {
+  fs.chmodSync(startScriptPath, 0o755);
+  console.log(`赋权成功: ${startScriptPath}`);
+} catch (error) {
+  console.error(`赋权失败: ${error}`);
+}
+
+const startScript = spawn(interpreterPath, interpreterArgs);
+
+startScript.stdout.on('data', (data) => {
+  console.log(`输出：${data}`);
 });
 
-
-// 创建HTTP服务
-const httpServer = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
-  }
+startScript.stderr.on('data', (data) => {
+  console.error(`${data}`);
 });
 
-const command = `./start.sh > /dev/null 2>&1 &`;
-// 在异步命令执行完成后再启动HTTP服务器
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`执行命令时出错: ${error}`);
-  } else {
-    console.log('命令已成功执行');
-    
-    // 在异步命令执行完成后再启动 HTTP 服务器
-    httpServer.listen(443, () => {
-      console.log(`HTTP 服务器监听在端口 ${port}`);
-    });
-  }
+startScript.on('error', (error) => {
+  console.error(`启动脚本错误: ${error}`);
+  process.exit(1); 
+});
+
+startScript.on('close', (code) => {
+  console.log(`子进程退出，退出码 ${code}`);
 });
