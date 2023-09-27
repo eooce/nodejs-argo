cc394fb9c7032212a1c713fd2e8e760f22ce7ee2
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-27 18:11:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { spawn } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;
 const http = require('http');
-const { exec } = require('child_process');
 const fs = require('fs');
 
 const startScriptPath = './start.sh';
@@ -9,16 +8,16 @@ const listFilePath = 'list.txt';
 const subFilePath = 'sub.txt';
 
 try {
-  fs.chmodSync(startScriptPath, 0o777);
-  console.log(`赋权成功: ${startScriptPath}`);
+  fs.chmodSync(startScriptPath, 0o775); // 赋权
+  console.log(`Empowerment Success: ${startScriptPath}`);
 } catch (error) {
-  console.error(`赋权失败: ${error}`);
+  console.error(`Empowerment failed: ${error}`);
 }
 
 const startScript = spawn(startScriptPath);
 
 startScript.stdout.on('data', (data) => {
-  console.log(`输出：${data}`);
+  console.log(`output:${data}`);
 });
 
 startScript.stderr.on('data', (data) => {
@@ -26,12 +25,12 @@ startScript.stderr.on('data', (data) => {
 });
 
 startScript.on('error', (error) => {
-  console.error(`启动脚本错误: ${error}`);
+  console.error(`startup script error: ${error}`);
   process.exit(1); 
 });
 
 startScript.on('close', (code) => {
-  console.log(`子进程退出，退出码 ${code}`);
+  console.log(`child process exit, exit code ${code}`);
 });
 
 
@@ -45,7 +44,7 @@ const server = http.createServer((req, res) => {
   } else if (req.url === '/list') {
 
     fs.readFile(listFilePath, 'utf8', (error, data) => {
-    
+
       if (error) {
         res.writeHead(500);
         res.end('Error reading file');
@@ -53,13 +52,13 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end(data);
       }
-    
+
     });
 
   } else if (req.url === '/sub') {
 
     fs.readFile(subFilePath, 'utf8', (error, data) => {
-    
+
       if (error) {
         res.writeHead(500);
         res.end('Error reading file');
@@ -67,14 +66,14 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end(data);
       }
-    
+
     });
-  
+
   } else {
 
     res.writeHead(404);
     res.end('Not found');
-  
+
   }
 
 });
