9e90c11c91f13698adc9d82cbac01e95d14345d8
eooce <142894633+eooce@users.noreply.github.com>
2024-02-26 12:55:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 const exec = require("child_process").exec;
-const subtxt = './temp/sub.txt' // 节点文件路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 7860; // 节点订阅端口，若无法订阅请改为分配的端口
+const subtxt = './temp/sub.txt' // 节点文件路径，需和start.sh中设置的文件夹对应，否则无法订阅
 
 // Run start.sh
 fs.chmod("start.sh", 0o777, (err) => {
-    if (err) {
-        console.error(`Failed to set execute permissions for start.sh`);
-        return;
-    }
-    console.log(`Set execute permissions for start.sh`);
-    const child = exec('bash start.sh');
-    child.stdout.on('data', (data) => {
-        console.log(data);
-    });
-    child.stderr.on('data', (data) => {
-        console.error(data);
-    });
+  if (err) {
+      console.error(`start.sh empowerment failed: ${err}`);
+      return;
+  }
+  console.log(`start.sh empowerment successful`);
+  const child = exec('bash start.sh');
+  child.stdout.on('data', (data) => {
+      console.log(data);
+  });
+  child.stderr.on('data', (data) => {
+      console.error(data);
+  });
+  child.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      console.clear()
+      console.log(`App is running`);
+  });
 });
 
 // create HTTP server
